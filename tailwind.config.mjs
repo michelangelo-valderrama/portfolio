e8bd7027045d8fc629d0a33163c54431b913f8d9
michelangelo-valderrama <michelangelovalderrama@gmail.com>
2024-03-24 12:08:51 +0100

    noise effect and frame

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				background: "hsl(var(--background))"
-			}
-		},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        border: "hsl(var(--border))",
+        muted: {
+          foreground: "hsl(var(--muted))",
+        },
+        accent: {
+          foreground: "hsl(var(--accent-foreground))",
+        },
+      },
+    },
+  },
+  plugins: [],
 }
