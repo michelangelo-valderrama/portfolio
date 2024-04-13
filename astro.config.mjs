c86e7f484d96155dc8050d9128bd4be7159be9f5
michelangelo-valderrama <michelangelovalderrama@gmail.com>
2024-04-13 18:02:21 +0200

    basic seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://portfolio.imangelo.dev",
   integrations: [tailwind()],
   devToolbar: {
     enabled: false,
