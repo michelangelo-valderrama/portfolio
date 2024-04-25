9c7aaa697745a8c72dce57c6a982c6da1d9c3ef8
michelangelo-valderrama <michelangelovalderrama@gmail.com>
2024-04-25 23:39:56 +0200

    more more more

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,12 +1,18 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs))
 }
 
 export function translateArr(arr: any[], steps: number = 0) {
-  const left = arr.slice(0, steps)
-  const right = arr.slice(steps)
-  return right.concat(left)
+	const left = arr.slice(0, steps)
+	const right = arr.slice(steps)
+	return right.concat(left)
 }
+
+export const $ = <T extends HTMLElement>(s: string) =>
+	document.querySelector<T>(s)!
+
+export const $$ = <T extends HTMLElement>(s: string) =>
+	document.querySelectorAll<T>(s)!
